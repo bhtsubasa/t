c3a6c46ea7de9258caabfdff141423b8394402a4
BiHui <bihui19870425@163.com>
2012-12-19 14:38:11 +0800

    add Desert tsx
    add Desert tsx

---
+++ Desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Desert" tilewidth="32" tileheight="32" spacing="1" margin="1">
+ <image source="../../../Volumes/Tiled 0.8.1/examples/tmw_desert_spacing.png" width="265" height="199"/>
+</tileset>
